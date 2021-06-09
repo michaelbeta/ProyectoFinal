6f7b186bb780a157a67a3bb7173fde358f3f4438
Michaelbeta <56772706+michaelbeta@users.noreply.github.com>
2021-06-08 20:06:38 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      colors:{
+        violet:"#15086B"
+      },
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
