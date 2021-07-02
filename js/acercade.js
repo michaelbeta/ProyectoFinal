9ec9ddb7836535bf753fab2befa3fd95bbb97c2c
Minor Blanco Chaves <minor.blancochaves@ucr.ac.cr>
2021-07-02 16:38:12 -0600

    Algunos Cambios

--- js/acercade.js
+++ js/acercade.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#btnSend').click(function(){
+    $('#btnSend').click(function () {
 
         var errores = '';
 
         // Validado Nombre ==============================
-        if( $('#names').val() == '' ){
+        if ($('#names').val() == '') {
             errores += '<p>Escriba un nombre</p>';
             $('#names').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#names').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Correo ==============================
-        if( $('#email').val() == '' ){
+        if ($('#email').val() == '') {
             errores += '<p>Ingrese un correo</p>';
             $('#email').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#email').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Mensaje ==============================
-        if( $('#mensaje').val() == '' ){
+        if ($('#mensaje').val() == '') {
             errores += '<p>Escriba un mensaje</p>';
             $('#mensaje').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#mensaje').css("border-bottom-color", "#d1d1d1")
         }
 
         // ENVIANDO MENSAJE ============================
-        if( errores == '' == false){
-            var mensajeModal = '<div class="modal_wrap">'+
-                                    '<div class="mensaje_modal">'+
-                                        '<h3>Errores encontrados</h3>'+
-                                        errores+
-                                        '<span id="btnClose">Cerrar</span>'+
-                                    '</div>'+
-                                '</div>'
+        if (errores == '' == false) {
+            var mensajeModal = '<div class="modal_wrap">' +
+                '<div class="mensaje_modal">' +
+                '<h3>Errores encontrados</h3>' +
+                errores +
+                '<span id="btnClose">Cerrar</span>' +
+                '</div>' +
+                '</div>'
 
             $('body').append(mensajeModal);
         }
 
         // CERRANDO MODAL ==============================
-        $('#btnClose').click(function(){
+        $('#btnClose').click(function () {
             $('.modal_wrap').remove();
         });
     });
